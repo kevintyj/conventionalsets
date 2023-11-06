63b63fc5226b52d439bc5e2265277ad17aafd65c
kevintyj <hello@kevintyj.com>
2023-11-06 11:27:26 +0530

    feat: Initial typescript commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import antfu from '@antfu/eslint-config';
+
+/* Using custom configuration of Antfu's code style: https://github.com/antfu/eslint-config */
+export default antfu(
+	{
+		typescript: {
+			tsconfigPath: 'tsconfig.json',
+		},
+		stylistic: {
+			indent: 'tab',
+			quotes: 'single',
+		},
+		ignores: [
+			'dist',
+			'.idea',
+		],
+	},
+	{
+		files: ['tsconfig.json', 'package.json'],
+		rules: {
+			'jsonc/sort-keys': 'off',
+		},
+	},
+	{
+		rules: {
+			'style/semi': ['error', 'always'],
+		},
+	},
+);
